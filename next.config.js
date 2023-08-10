c247213f8fa901dd59c4ff2422c7111738ce52c8
luolp <1143079030@qq.com>
2023-08-10 18:29:02 +0800

    修改next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,30 @@ module.exports = {
   images: {
     domains: ["data.eminstore.com"],
   },
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "x-content-type-options",
+                        value: "nosniff",
+                    },
+                    { key: "x-xss-protection", value: "1" },
+                    { key: "x-frame-options", value: "DENY" },
+                    {
+                        key: "strict-transport-security",
+                        value: "max-age=31536000; includeSubDomains",
+                    },
+                ],
+            },
+
+            {
+                source: "/checkout/(.*)",
+                headers: [{ key: "x-frame-options", value: "ALLOWALL" }],
+            },
+        ];
+    },
   async rewrites() {
       return [
           {
