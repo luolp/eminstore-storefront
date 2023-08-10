56dc7bd2b0b7641f408096dda24f0c3599db5935
luolp <1143079030@qq.com>
2023-08-10 18:19:56 +0800

    修改start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+# 获取正在监听 3000 端口的进程号
+PORT=3000
+PID=$(lsof -t -i:$PORT)
+
+# 如果进程号存在，则杀死进程
+if [ -n "$PID" ]; then
+  echo "Killing process on port $PORT: $PID"
+  kill $PID
+fi
+
 nohup yarn start >/opt/eminstore/log/storefront-run.log 2>&1 &
