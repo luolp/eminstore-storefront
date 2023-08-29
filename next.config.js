5b599142e84f5e8b3ab68eccba2da8be017daa33
luolp <1143079030@qq.com>
2023-08-30 00:04:33 +0800

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,21 +7,6 @@ module.exports = {
     async headers() {
         return [
             {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "x-content-type-options",
-                        value: "nosniff",
-                    },
-                    { key: "x-xss-protection", value: "1" },
-                    { key: "x-frame-options", value: "DENY" },
-                    {
-                        key: "strict-transport-security",
-                        value: "max-age=31536000; includeSubDomains",
-                    },
-                ],
-            },
-            {
                 source: "/checkout/(.*)",
                 headers: [{ key: "x-frame-options", value: "ALLOWALL" }],
             },
