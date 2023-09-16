84a5bfb6cecd948febcbd1b039c022c03f5348af
luolp <1143079030@qq.com>
2023-09-16 20:59:00 +0800

    测试

--- lib/useOrder.ts
+++
@@ -1,9 +0,0 @@
-import { OrderFragment, useOrderQuery } from "@/saleor/api";
-
-export const useOrder = (id: string) => {
-  const [{ data, fetching: loading }] = useOrderQuery({
-    variables: { languageCode: 'EN_US', id },
-  });
-
-  return { order: data?.order as OrderFragment, loading };
-};
